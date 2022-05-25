d5582b3c7ef3e05861127c3a5034008372e77f6f
Alexander Lucano <alrlucano@gmail.com>
2022-05-25 13:02:15 -0500

    Agregamos estilos a listado Pacientes

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,33 @@
+const Paciente = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Nombre: {""}
+        <span className="font-normal normal-case">Hook</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Propietario: {""}
+        <span className="font-normal normal-case">Alexander</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Email: {""}
+        <span className="font-normal normal-case">correo@adsjkas-.com</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Fecha Alta: {""}
+        <span className="font-normal normal-case">10 Diciembre de 2022</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Sintomas: {""}
+        <span className="font-normal normal-case">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati
+          libero repellendus, repellat doloremque dolorem accusantium, velit
+          quidem perferendis dolores cumque quis excepturi magnam deleniti
+          voluptate neque voluptates dignissimos provident? Officiis.
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default Paciente;
